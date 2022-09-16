605fcf19004f801837cb7fc34c36e90d40848451
noah daher <noah.daher@mths.ca>
2022-09-16 15:05:16 +0000

    Made Changes

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+// Copyright(c) 2022 Noah Daher All rights reserved
+//
+// Created by: Noah Daher
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+    int main() {
+    std::cout << "Hello, World!";
+
+    std::cout << "\ndone.";
+}
